a1d01a3b09d43b06de704c07219d02658cf5d1e9
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-01-12 14:30:11 -0700

    Add project files.

---
+++ HelloHackerRank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloHackerRank
+{
+    class Program
+    {
+        // .NET Framework 4.6
+        static void Main(string[] args)
+        {
+            string hello = "Hello World";
+            Console.WriteLine($"{hello}");
+            Console.ReadLine();
+        }
+    }
+}
