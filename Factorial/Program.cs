fa3ed12c660a1ce2acf3da9414d24cf8cf77eb9d
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 10:20:01 -0700

    Factorial committed

---
+++ Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactorialIterative(10);
+            FactorialRecursive(10);
+            Console.ReadLine();
+        }
+
+        static int FactorialIterative(int num)
+        {
+            int prod = 1;
+
+            for (int i = num; i > 0; --i)
+            {
+                prod *= (i);
+            }
+
+            return prod;
+        }
+
+        static int FactorialRecursive(int num)
+        {
+            return num == 0 ? 1 : num * FactorialRecursive(num - 1);
+        }
+    }
+}
