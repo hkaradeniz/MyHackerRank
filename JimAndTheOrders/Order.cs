0b01008df06f71bebabaaa02bdc295a056a20fd5
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 11:35:22 -0700

    Jim and the Orders committed

---
+++ JimAndTheOrders/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace JimAndTheOrders
+{
+    class Order
+    {
+        public int Sequence { get; set; }
+        public int pTime { get; set; }
+        public int oTime { get; set; }
+
+        public Order()
+        {
+            pTime = 0;
+            oTime = 0;
+            Sequence = 0;
+        }
+
+        public int TotalTime()
+        {
+            return pTime + oTime;
+        }
+    }
+}
---
+++ JimAndTheOrders/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace JimAndTheOrders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+            Order[] ar = new Order[orders];
+
+            for (int i = 0; i < orders; i++)
+            {
+                string[] times = Console.ReadLine().Split(' ');
+                Order order = new Order();
+                order.oTime = int.Parse(times[0]);
+                order.pTime = int.Parse(times[1]);
+                order.Sequence = i + 1;
+                ar[i] = order;
+            }
+
+            Sort(ar);
+        }
+
+        static void Sort(Order[] ar)
+        {
+            int arraySize = ar.Length;
+            int j;
+            Order element;
+
+            for (int i = 1; i < arraySize; i++)
+            {
+                element = ar[i];
+                j = i;
+                while (j > 0 && ar[j - 1].TotalTime() > element.TotalTime())
+                {
+                    ar[j] = ar[j - 1];
+                    ar[j - 1] = element;
+                    j--;
+                }
+            }
+
+            foreach (var item in ar)
+            {
+                Console.Write(item.Sequence + " ");
+            }
+        }
+    }
+}
