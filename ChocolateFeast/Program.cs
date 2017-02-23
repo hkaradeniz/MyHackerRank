090ea5dde6e3fb7582d8762eb06b4405e13a04be
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 10:33:46 -0700

    Floyds Cycle Finding Algorithm committed

--- ChocolateFeast/Program.cs
+++ ChocolateFeast/Program.cs
@@ -32,5 +32,6 @@ namespace ChocolateFeast
 
                 Console.WriteLine(totalChocolate);
             }
+        }
     }
 }
---
+++ FloydsCycleFindingAlgorithm/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace FloydsCycleFindingAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        // Floyd's cycle finding algorithm
+        public bool HasLoop(Node first)
+        {
+            if (first == null) // List doesn't exist, so loop either
+                return false;
+
+            Node slow, fast;
+            slow = fast = first;
+
+            while (true)
+            {
+                slow = slow.Next; // 1 Hop
+                if (fast.Next != null)
+                    fast = fast.Next.Next; // 2 Hops
+                else
+                    return false; // Next node is null, no loop
+
+                // If they hit null, no loop
+                if (slow == null || fast == null)
+                    return false;
+
+                if (slow == fast)
+                    return true; // The two meet, there is a loop
+
+            }
+        }
+
+        public class Node
+        {
+            public Node Next { get; set; }
+        }
+    }
+}
