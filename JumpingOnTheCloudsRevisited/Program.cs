fb1a14a1c8b29e0514731d22dabb26771284f38a
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 13:17:06 -0700

    Jumping on the Clouds: Revisited committed

---
+++ JumpingOnTheCloudsRevisited/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JumpingOnTheCloudsRevisited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] c_temp = Console.ReadLine().Split(' ');
+            int[] c = Array.ConvertAll(c_temp, Int32.Parse);
+
+            int thundercloud = 0;
+
+            // 9 3
+            // 0 0 1 0 0 1 1 0 0
+            // 92
+            for (int i = 0; i < n; i += k)
+            {
+                thundercloud += c[i];
+            }
+
+            Console.WriteLine(100 - (thundercloud * 2) - n / k);
+        }
+    }
+}
