2bb0fba62f7c39341a2aa090c7249fd01320f9a2
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 15:58:34 -0700

    Stack with Queue committed

---
+++ StackWithQueue/StackWithQueue.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace StackWithQueue
+{
+    class StackWithQueue
+    {
+        Queue<string> queue1 = new Queue<string>();
+        Queue<string> queue2 = new Queue<string>();
+
+        public void Push(string item)
+        {
+            if (queue1.Count == 0)
+            {
+                queue1.Enqueue(item);
+
+                while (queue2.Count > 0)
+                {
+                    queue1.Enqueue(queue2.Dequeue());
+                }
+            }
+            else
+            {
+                queue2.Enqueue(item);
+
+                while (queue1.Count > 0)
+                {
+                    queue2.Enqueue(queue1.Dequeue());
+                }
+            }
+        }
+
+        public string Pop()
+        {
+            if (queue1.Count > 0)
+            {
+                return queue1.Dequeue();
+            }
+            else
+            {
+                return queue2.Dequeue();
+            }
+        }
+    }
+}
