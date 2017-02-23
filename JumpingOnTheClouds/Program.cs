105c24853cc7b4b118840accbca9f62ef821815f
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 11:42:34 -0700

    Jumping on the Clouds committed

---
+++ JumpingOnTheClouds/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JumpingOnTheClouds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] c_temp = Console.ReadLine().Split(' ');
+            int[] c = Array.ConvertAll(c_temp, Int32.Parse);
+
+            int jump = 0;
+            int pointer = 0;
+
+            while (pointer < n - 1)
+            {
+                if (pointer + 2 < n && c[pointer + 2] == 0)
+                    pointer = pointer + 2;
+                else
+                    pointer++;
+
+                jump++;
+            }
+
+            Console.WriteLine(jump);
+        }
+    }
+}
