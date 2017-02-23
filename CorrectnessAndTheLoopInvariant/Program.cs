5b4ca8ded3d34ecef238ae00a395e9a147eb238e
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 14:10:01 -0700

    Correctness and the Loop Invariant committed

---
+++ CorrectnessAndTheLoopInvariant/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace CorrectnessAndTheLoopInvariant
+{
+    class Program
+    {
+        static void insertionSort(int[] A)
+        {
+            var j = 0;
+            for (var i = 1; i < A.Length; i++)
+            {
+                var value = A[i];
+                j = i;
+                while (j > 0 && value < A[j - 1])
+                {
+                    A[j] = A[j - 1];
+                    A[j - 1] = value;
+                    j--;
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", A));
+        }
+
+        static void Main(string[] args)
+        {
+            // int[] arr = { 1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9, 0 };
+            Console.ReadLine();
+            int[] _ar = (from s in Console.ReadLine().Split() select Convert.ToInt32(s)).ToArray();
+            insertionSort(_ar);
+        }
+    }
+}
