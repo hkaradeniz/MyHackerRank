7f301837f6d609af78f1008433bc0e5711a9cbdc
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 11:21:38 -0700

    Ice Cream Parlor committed

---
+++ IceCreamParlor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IceCreamParlor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tc = int.Parse(Console.ReadLine());
+
+            for (int k = 0; k < tc; k++)
+            {
+                int money = int.Parse(Console.ReadLine());
+                int flavorCount = int.Parse(Console.ReadLine());
+
+                string[] strArray = Console.ReadLine().Split(' ');
+                int[] flavors = Array.ConvertAll(strArray, Int32.Parse);
+                bool found = false;
+
+                for (int i = 0; i < flavorCount; i++)
+                {
+                    for (int j = i + 1; j < flavorCount; j++)
+                    {
+                        if (flavors[i] + flavors[j] == money)
+                        {
+                            Console.WriteLine((i + 1) + " " + (j + 1));
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (found)
+                        break;
+
+                }
+            }
+        }
+    }
+}
