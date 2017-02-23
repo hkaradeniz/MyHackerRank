6bbd7b07f737df78adbd4c97a63e45275d1fc0cf
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 16:00:49 -0700

    Staircase committed

---
+++ Staircase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n > 0)
+            {
+                for (int i = 1; i < n + 1; i++)
+                {
+                    for (int space = 1; space < n - i + 1; space++)
+                        Console.Write(" ");
+                    for (int star = n - i + 1; star < n + 1; star++)
+                        Console.Write("#");
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
