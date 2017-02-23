226930eedd7e81f7e235c2cb45d2b741f1c643d5
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 13:47:53 -0700

    Mars Exploration committed

---
+++ MarsExploration/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MarsExploration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            int numOfMessages = S.Length / 3;
+            int changedChars = 0;
+
+            //SOSSPSSQSSOR
+            for (int i = 0; i < numOfMessages; i++)
+            {
+                string msg = S.Substring(i * 3, 3);
+                if (msg != "SOS")
+                {
+                    if (msg[0] != 'S')
+                        changedChars++;
+
+                    if (msg[1] != 'O')
+                        changedChars++;
+
+                    if (msg[2] != 'S')
+                        changedChars++;
+                }
+            }
+
+            Console.WriteLine(changedChars);
+        }
+    }
+}
