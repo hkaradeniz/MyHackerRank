64a9468aa0d9ceb0c8fce9ec19cc3a8d81f35a81
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 09:57:41 -0700

    Divisible Sum Pairs committed

---
+++ DivisibleSumPairs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DivisibleSumPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+            //1 3 2 6 1 2
+            int counter = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+                for (int j = i + 1; j < a.Length; j++)
+                    if ((a[i] + a[j]) % k == 0)
+                        counter++;
+
+            Console.WriteLine(counter);
+        }
+    }
+}
