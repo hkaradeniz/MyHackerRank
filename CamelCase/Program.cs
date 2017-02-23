d7e582b7d967cecbd9644bfe63e1b98fb3d1a1e5
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 09:21:17 -0700

    CamelCase committed

---
+++ CamelCase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            Console.WriteLine(s.Count(c => char.IsUpper(c)) + 1);
+        }
+    }
+}
