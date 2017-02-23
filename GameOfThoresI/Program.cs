c36ab1ab66bd67f442efc03bbfd344bf52cd4d43
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 10:57:51 -0700

    Game of Thrones - I committed

---
+++ GameOfThoresI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameOfThoresI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            string stdin = Console.ReadLine();
+
+            char[] array = stdin.ToLower().ToCharArray();
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach (char character in array)
+            {
+                if (dict.ContainsKey(character))
+                {
+                    dict[character]++;
+                }
+                else
+                {
+                    dict[character] = 1;
+                }
+            }
+
+            int counter = 0;
+            foreach (var pair in dict)
+            {
+                int result = pair.Value % 2;
+
+                if (result > 0)
+                    counter++;
+
+                if (counter > 1)
+                {
+                    Console.Write("NO");
+                    return;
+                }
+            }
+
+            Console.Write("YES");
+        }
+    }
+}
