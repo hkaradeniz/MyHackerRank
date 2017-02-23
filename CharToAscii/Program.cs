a178f03a08586664aaeb1f76ec88abc83d30212f
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-23 09:25:10 -0700

    CharToAscii committed

---
+++ CharToAscii/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CharToAscii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Huseyin7";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int asciivalue = (char)text[i];
+                Console.WriteLine("> {0}", asciivalue);
+            }
+        }
+    }
+}
