13eda6cd7538c83ac260d41c86e245aad0d3d8d2
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-24 08:30:26 -0700

    String Construction committed

---
+++ StringConstruction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringConstruction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < n; a0++)
+            {
+                string s = Console.ReadLine();
+
+                var hash = new HashSet<char>();
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char c = s[i];
+                    if (!hash.Contains(c))
+                        hash.Add(c);
+                }
+
+                Console.WriteLine(hash.Count);
+                hash.Clear();
+            }
+        }
+    }
+}
