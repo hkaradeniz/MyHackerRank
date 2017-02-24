b874843a44400bc3ac38ea34ddf51ba155f33375
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-24 08:34:45 -0700

    Super Reduced String committed

---
+++ SuperReducedString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SuperReducedString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            string str = Console.ReadLine();
+            int pointer = 0;
+
+            while (pointer < str.Length - 1)
+            {
+                if (str[pointer] == str[pointer + 1])
+                {
+                    str = str.Remove(pointer, 2);
+
+                    if (pointer > 0)
+                        pointer--;
+                }
+                else
+                {
+                    pointer++;
+                }
+            }
+
+            if (string.IsNullOrEmpty(str))
+                Console.WriteLine("Empty String");
+            else
+                Console.WriteLine(str);
+        }
+    }
+}
