38e93d5a5bf9bf8c36825af7d04e050e871ae610
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-02-24 09:36:06 -0700

    Utopian Tree committed

---
+++ UtopianTree/MyTree.cs
@@ -0,0 +1,27 @@
+﻿namespace UtopianTree
+{
+    class MyTree
+    {
+        int Height;
+
+        public MyTree()
+        {
+            Height = 1;
+        }
+
+        public void GrowSpring()
+        {
+            Height = Height * 2;
+        }
+
+        public void GrowSummer()
+        {
+            Height = Height + 1;
+        }
+
+        public int GetTreeHeight()
+        {
+            return Height;
+        }
+    }
+}
---
+++ UtopianTree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UtopianTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                MyTree myTree = new MyTree();
+
+                for (int j = 1; j <= n; j++)
+                {
+                    if (j % 2 == 1)
+                        myTree.GrowSpring();
+                    else
+                        myTree.GrowSummer();
+                }
+
+                Console.WriteLine(myTree.GetTreeHeight());
+            }
+        }
+    }
+}
